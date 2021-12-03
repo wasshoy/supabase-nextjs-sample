4692ce4b2b02d4433465bf58db3118eef9fa167a
wasshoy <404ari@gmail.com>
2021-12-03 22:38:34 +0900

    :sparkles: Supabase の設定を追加

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,7 @@
 import 'tailwindcss/tailwind.css'
 
-export default MyApp({ Component, pageProps }) => {
+function MyApp({ Component, pageProps }) => {
     return <Component {...pageProps}/>
-}
+};
+
+export default MyApp;
